86914e5f409fa762e5674536ea2951b4b8dc3133
Renato Santos <renato.santos98@hotmail.com>
2022-04-18 14:16:13 +0100

    Exercise 2.3

--- ex02/ex_02_03.py
+++ ex02/ex_02_03.py
@@ -1,6 +1,6 @@
 # Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
 
-hrs = float(input('Enter Hours: '))
-rph = float(input('Enter Rate: '))
+hrs = float(input("Enter Hours: "))
+rph = float(input("Enter Rate: "))
 pay = round(hrs * rph, 2)
-print('Pay:', pay)
+print("Pay:", pay)
