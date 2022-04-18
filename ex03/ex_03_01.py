b3328d28be3dfaaa2e7f16ebb409ed1c7c320a02
Renato Santos <renato.santos98@hotmail.com>
2022-04-18 14:16:42 +0100

    Exercise 3.1

--- ex03/ex_03_01.py
+++ ex03/ex_03_01.py
@@ -1,9 +1,9 @@
 # Exercise 1: Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
 
-hrs = float(input('Enter Hours: '))
-rph = float(input('Enter Rate: '))
+hrs = float(input("Enter Hours: "))
+rph = float(input("Enter Rate: "))
 if hrs > 40:
     pay = round(40 * rph + (hrs - 40) * rph * 1.5, 2)
 else:
     pay = round(hrs * rph, 2)
-print('Pay:', pay)
+print("Pay:", pay)
