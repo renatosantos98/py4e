e5872069009238ccb9b6182b3c42f9c56560931f
Renato Santos <renato.santos98@hotmail.com>
2022-04-18 17:18:31 +0100

    Exercise 5.1
    Exercise 5.1

--- ex05/ex_05_01.py
+++ ex05/ex_05_01.py
@@ -2,31 +2,26 @@
 # Once "done" is entered, print out the total, count, and average of the numbers.
 # If the user enters anything other than a number, detect their mistake using try and except and print an error message and skip to the next number.
 
-number_list = []
-
-while True:
-    line = input("Enter a number, or type 'done' to finish: ")
-    if line == "done":
-        break
-    else:
-        try:
-            new_number = float(line)
-        except:
-            print(
-                "Error: Invalid Data. Please enter a number or type 'done' to finish."
-            )
-            continue
-        else:
-            number_list.append(new_number)
-
 total = 0
 count = 0
 
-for number in number_list:
-    total = total + number
-    count = count + 1
-
-average = total / count
+while True:
+    line = input("Enter a number, or type 'done' to finish: ")
+    if line == "done" and count == 0:
+        print("Error: Please enter at least one number.")
+        continue
+    elif line == "done":
+        break
+    else:
+        try:
+            number = float(line)
+        except:
+            print("Error: Invalid Data. Enter a number, or type 'done' to finish.")
+            continue
+        else:
+            total = total + number
+            count = count + 1
+            average = total / count
 
 print("Total: ", total)
 print("Count: ", count)
