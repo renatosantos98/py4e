f0d4daf2dcf06a06bc14d2c068fe84d1022a96d6
Renato Santos <renato.santos98@hotmail.com>
2022-04-18 10:18:41 +0100

    Exercise 4.6

---
+++ ex04/ex_04_06.py
@@ -0,0 +1,22 @@
+# Rewrite your pay computation with time-and-a-half for overtime and create a function called computepay which takes two parameters (hours and rate).
+
+
+def computepay(hrs, rph):
+    try:
+        hrs = float(hrs)
+        rph = float(rph)
+    except:
+        print("Error, please enter numeric input")
+        quit()
+    else:
+        if hrs > 40:
+            pay = round(40 * rph + (hrs - 40) * rph * 1.5, 2)
+            return pay
+        else:
+            pay = round(hrs * rph, 2)
+            return pay
+
+
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+print(computepay(hours, rate))
